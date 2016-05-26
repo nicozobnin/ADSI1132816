ae2b53a5caf0b4877d58193f3d364077f81d4804
nicozobnin <nicozobnin10b@gmail.com>
2016-05-26 10:30:26 -0500

    ejercicios sexys y hermosos

---
+++ html/html/codigo.js
@@ -0,0 +1,9 @@
+
+var nombre = prompt ('cual es su nombre¡?');
+
+if (nombre == 'oliver') {
+	alert('bienvenido: '+nombre+'el goleador');
+} else {
+	alert('bienvenido: '+nombre+' el defensa');
+}
+
